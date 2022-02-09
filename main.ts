9e2fb2f6f4059881cebe5ab037c5bd714f3ba31f
maciatko1979 <99343414+maciatko1979@users.noreply.github.com>
2022-02-09 16:37:10 +0100

    Počiatočné súbory pre projekt MakeCode

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onPinPressed(TouchPin.P1, function () {
+	
+})
+basic.showString("Katarína")
+basic.showString("pani")
+basic.showString("Kvietočková")
+music.playMelody("- - A A C5 G C5 A ", 120)
+basic.showArrow(ArrowNames.NorthEast)
+basic.showLeds(`
+    # # . # #
+    # # # # #
+    # # # # #
+    . # # # .
+    . . # . .
+    `)
+basic.forever(function () {
+	
+})
