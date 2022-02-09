d717c9e0193527d6ff9811da6e2975e5e0ea3eb8
maciatko1979 <99343414+maciatko1979@users.noreply.github.com>
2022-02-09 17:49:55 +0100

    Aktualizovať main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ input.onButtonPressed(Button.A, function () {
     basic.showString("Katarína")
     basic.showString("pani")
     basic.showString("Kvietočková")
-    music.playMelody("- - A A C5 G C5 A ", 120)
+    music.playMelody("C5 B A G F E D C ", 120)
     basic.showArrow(ArrowNames.NorthEast)
     basic.showLeds(`
         # # . # #
@@ -16,7 +16,7 @@ input.onButtonPressed(Button.B, function () {
     basic.showString("Oskar!")
     basic.showString("pán")
     basic.showString("Kvietocek")
-    music.playMelody("- - B A B C5 E C ", 120)
+    music.playMelody("C5 B G E D C C C ", 120)
     basic.showArrow(ArrowNames.SouthWest)
     basic.showIcon(IconNames.TShirt)
 })
