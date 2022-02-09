35aaf9943c947d5d167196ff039416c54dda2e58
maciatko1979 <99343414+maciatko1979@users.noreply.github.com>
2022-02-09 17:27:42 +0100

    Aktualizovať pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,25 @@
-basic.showString("Katarína")
-basic.showString("pani")
-basic.showString("Kvietočková")
-music.playMelody("- - A A C5 G C5 A ", 120)
-basic.showArrow(ArrowNames.NorthEast)
-basic.showLeds(`
-    # # . # #
-    # # # # #
-    # # # # #
-    . # # # .
-    . . # . .
-    `)
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Katarína")
+    basic.showString("pani")
+    basic.showString("Kvietočková")
+    music.playMelody("- - A A C5 G C5 A ", 120)
+    basic.showArrow(ArrowNames.NorthEast)
+    basic.showLeds(`
+        # # . # #
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Oskar!")
+    basic.showString("pán")
+    basic.showString("Kvietocek")
+    music.playMelody("- - B A B C5 E C ", 120)
+    basic.showArrow(ArrowNames.SouthWest)
+    basic.showIcon(IconNames.TShirt)
+})
+basic.forever(function () {
+	
+})
